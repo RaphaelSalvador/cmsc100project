06597b0b4d788837d1473392d0bf3922fa687ea7
RaphaelSalvador <rmsalvador2@up.edu.ph>
2021-12-27 10:51:12 +0000

    feat: make a running server

---
+++ src/services/base/home.js
@@ -0,0 +1,13 @@
+/**
+ * This is the home function
+ * 
+ * @param {*} request 
+ * @param {*} reply 
+ * @returns {*}
+ */
+
+export async function home (request, reply) {
+    return {
+        hello: 'world from home'
+    }
+};
