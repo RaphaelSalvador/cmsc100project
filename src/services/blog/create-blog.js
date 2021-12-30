8155ccb6ebb1dba5895c778075d01b530d791dd9
RaphaelSalvador <rmsalvador2@up.edu.ph>
2021-12-30 09:50:25 +0000

    feat: add security

--- src/services/blog/create-blog.js
+++ src/services/blog/create-blog.js
@@ -9,14 +9,16 @@ import { models } from '../../utils/mongodb/index.js';
  */
 
  export async function createBlog (request, reply) {
-    const { body } = request;
+    const { body, user } = request;
     const { title, text } = body;
+    const { username } = user;
 
     const { Blog } = models;
 
     const blog = new Blog({
         title,
-        text
+        text,
+        username
     });
 
     await blog.save();
--- src/services/blog/delete-blog.js
+++ src/services/blog/delete-blog.js
@@ -5,14 +5,18 @@ import { models } from '../../utils/mongodb/index.js';
  *
  */
 
- export async function deleteBlog (request) {
-    const { params } = request;
-
+ export async function deleteBlog (request, reply) {
+    const { params, user } = request;
     const { id: _id } = params;
+    const { username } = user;
     
     const { Blog } = models;
 
-    await Blog.deleteOne({ _id });
+    const blog = await Blog.deleteOne({ _id, username });
+
+    if (blog.deletedCount === 0 ) {
+      return reply.notFound();
+    }
 
     return {
         success: true
